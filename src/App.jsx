ccfa058fe9ce473fb97e353b72b11dfaa1fff1bc
Amit <amitkm.4211@gmail.com>
2024-03-16 23:31:28 +0530

    Nav, Hero Section, Done

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+// import { useState } from 'react'
+import {
+	About,
+	Contact,
+	Experience,
+	Feedbacks,
+	Hero,
+	Navbar,
+	Tech,
+	Works,
+	StarsCanvas,
+} from './components';
+
+const App = () => {
+	return (
+		<div className="relative z-0 bg-primary">
+			<div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+				<Navbar />
+				<Hero />
+			</div>
+			<About />
+			<Tech />
+			<Experience />
+			<Works />
+			<Feedbacks />
+			<div className="relative z-0">
+				<Contact />
+				<StarsCanvas />
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import { Tilt } from 'react-tilt';
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+
+const About = () => {
+	return (
+		<>
+			<h1>About</h1>
+		</>
+	);
+};
+
+export default About;
